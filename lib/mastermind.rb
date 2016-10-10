b7049bbec815e6554f51eb46091e0fdb6dd5cc71
Brett Rivers <riverswb@gmail.com>
2016-10-10 17:41:17 -0600

    most of game flow complete

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -1,10 +1,30 @@
 require 'pry'
 class Mastermind
+  attr_reader :answer, :guess
+  def initialize
+    @answer = ""
+    @guess = ""
+  end
 
-  def get_guess
-    guess = gets.chomp.downcase
+
+  def get_guess(guess = nil)
+    if guess == nil
+      @guess = gets.chomp.downcase
+    else @guess = guess.downcase
+    end
+    guess_actions
+  end
+
+  def guess_actions
     if guess == "q" || guess == "quit"
       quit_mastermind
+    elsif guess == "c" || guess == "cheat"
+      get_cheat
+    elsif guess.length < sequence.length
+      p "Too short!"
+    elsif guess.length > sequence.length
+      p"Too long!"
+    else puts "I don't know how to #{guess}."
     end
   end
 
@@ -13,7 +33,12 @@ class Mastermind
   end
 
   def get_cheat
+    puts answer
+  end
 
+  def sequence
+    possible = ["r","g","b","y"]
+    @answer = (possible * 4).shuffle.take(4).join("")
   end
 
 
--- test/mastermind_test.rb
+++ test/mastermind_test.rb
@@ -2,7 +2,7 @@ require 'minitest/autorun'
 require 'minitest/emoji'
 require './lib/mastermind.rb'
 require './lib/instructions.rb'
-
+require "pry"
 class MastermindTest < Minitest::Test
 
   def test_mastermind_exists
@@ -10,37 +10,50 @@ class MastermindTest < Minitest::Test
   end
 
   def test_user_can_make_a_guess
-    skip
     mm = Mastermind.new
-
-    assert_equal "q" , mm.get_guess
+    mm.get_guess("t")
+    assert_equal "t" , mm.guess
   end
 
   def test_guess_is_case_insensitive
-    skip
-    mm = Mastermind.new
 
-    assert_equal "q" , mm.get_guess
+    mm = Mastermind.new
+    mm.get_guess("QpRT")
+
+    assert_equal "qprt" , mm.guess
   end
 
   def test_if_guess_is_q_or_quit_exit_game
-    skip
+    # skip
     mm = Mastermind.new
-    mm.get_guess
+    mm.get_guess("q")
 
     assert_equal "Good Bye!", mm.quit_mastermind
   end
 
   def test_it_has_a_sequence
+    mm = Mastermind.new
 
+    assert_equal 4, mm.sequence.length
   end
 
   def test_if_guess_is_c_or_cheat_print_the_sequence
-    skip
     mm = Mastermind.new
-    mm.get_guess
+    mm.get_guess("c")
 
-    assert mm.get_cheat
+    assert_equal mm.sequence, mm.answer
+  end
+
+  def test_knows_if_guess_is_too_short
+    mm = Mastermind.new
+
+    assert_equal "Too short!", mm.get_guess("s")
+  end
+
+  def test_it_knows_if_it_is_too_long
+    mm = Mastermind.new
+
+    assert_equal "Too long!", mm.get_guess("ssssss")
   end
 
 end
