7152cc6a47cff40db674f0f6563639692b6ccccd
Brett Rivers <riverswb@gmail.com>
2016-10-11 20:20:11 -0600

    testing, testing, testing

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -1,17 +1,18 @@
 require 'pry'
 class Mastermind
-  attr_reader :answer, :guess
+  attr_reader :answer,:guess_count, :positions, :elements
+  attr_accessor :guess, :answer
   def initialize
     @answer = ""
-    @guess = ""
+    @guess = guess
+    @guess_count = 0
+    @elements = 0
+    @positions = 0
   end
 
 
-  def get_guess(guess = nil)
-    if guess == nil
-      @guess = gets.chomp.downcase
-    else @guess = guess.downcase
-    end
+  def get_guess
+    @guess = gets.chomp.downcase
     guess_actions
   end
 
@@ -20,25 +21,30 @@ class Mastermind
       quit_mastermind
     elsif guess == "c" || guess == "cheat"
       get_cheat
+      # get_guess
     elsif guess == answer
       end_game
     elsif guess.length < sequence.length
       p "Too short!"
+      # get_guess
     elsif guess.length > sequence.length
       p "Too long!"
+      # get_guess
     elsif guess != answer
         feedback
     else
       puts "I don't know how to #{guess}."
     end
+    # get_guess
   end
 
   def quit_mastermind
-    "Good Bye!"
+    p "Good Bye!"
   end
 
   def get_cheat
     puts answer
+    p "Cheater!"
   end
 
   def sequence
@@ -47,13 +53,28 @@ class Mastermind
   end
 
   def end_game
+    @guess_count += 1
     p "Congratulations!"
   end
 
   def feedback
-    p "'RRGB' has 3 of the correct elements with 2 in the correct positions
-        You've taken 1 guess"
-  end
+    @guess_count += 1
+    sequence = answer.chars
+    guess_colors = guess.chars
+    guess_colors.each do |color|
+      sequence.include?(color)
+      (p = sequence.find_index(color)) && sequence.delete_at(p)
+    end
+    @elements = answer.length - sequence.length
+    @positions = guess.chars.zip(answer.chars).count do |n|
+      n[0] == n[1]
+    end
 
+    p "'#{guess.upcase}' has #{elements} of the correct elements with #{positions} in the correct positions"
+    p  "You've taken #{guess_count} guess"
+    p "feedback"
+    # get_guess
+  end
+# Mastermind.new.get_guess
 
 end
--- test/mastermind_test.rb
+++ test/mastermind_test.rb
@@ -1,5 +1,5 @@
 require 'minitest/autorun'
-require 'minitest/emoji'
+require 'minitest/pride'
 require './lib/mastermind.rb'
 require './lib/instructions.rb'
 require "pry"
@@ -9,26 +9,11 @@ class MastermindTest < Minitest::Test
     assert Mastermind.new
   end
 
-  def test_user_can_make_a_guess
-    mm = Mastermind.new
-    mm.get_guess("t")
-    assert_equal "t" , mm.guess
-  end
-
-  def test_guess_is_case_insensitive
-
-    mm = Mastermind.new
-    mm.get_guess("QpRT")
-
-    assert_equal "qprt" , mm.guess
-  end
-
   def test_if_guess_is_q_or_quit_exit_game
-    # skip
     mm = Mastermind.new
-    mm.get_guess("q")
+    mm.guess = "q"
 
-    assert_equal "Good Bye!", mm.quit_mastermind
+    assert_equal "Good Bye!", mm.guess_actions
   end
 
   def test_it_has_a_sequence
@@ -36,38 +21,103 @@ class MastermindTest < Minitest::Test
 
     assert_equal 4, mm.sequence.length
   end
-
+  #
   def test_if_guess_is_c_or_cheat_print_the_sequence
     mm = Mastermind.new
-    mm.get_guess("c")
+    mm.guess = ("c")
 
-    assert_equal mm.sequence, mm.answer
+    assert_equal "Cheater!", mm.guess_actions
   end
 
   def test_knows_if_guess_is_too_short
     mm = Mastermind.new
+    mm.guess = ("s")
 
-    assert_equal "Too short!", mm.get_guess("s")
+    assert_equal "Too short!", mm.guess_actions
   end
 
   def test_it_knows_if_it_is_too_long
     mm = Mastermind.new
+    mm.guess = ("ssssss")
 
-    assert_equal "Too long!", mm.get_guess("ssssss")
+    assert_equal "Too long!", mm.guess_actions
   end
 
   def test_it_goes_to_end_game_if_guess_equals_answer
     mm = Mastermind.new
-    mm.get_guess("c")
-    mm.get_guess(mm.answer)
+    mm.guess = "rgby"
+    mm.answer = "rgby"
 
-    assert mm.end_game
+    assert_equal "Congratulations!", mm.guess_actions
   end
 
   def test_it_goes_to_feedback_if_guess_is_incorrect
     mm = Mastermind.new
-    mm.get_guess("gggg")
+    mm.guess = "grby"
+    mm.answer = "rgyb"
 
-    assert mm.feedback
+    assert_equal ("feedback"), mm.guess_actions
+  end
+
+
+  def test_feedback_can_calculcate_the_number_of_correct_elements_
+    mm = Mastermind.new
+    mm.answer = "rgby"
+    mm.guess = "bgby"
+    mm.feedback
+
+    assert_equal 3, mm.elements
+  end
+
+  def test_it_can_tell_how_many_are_in_correct_position
+    mm = Mastermind.new
+    mm.answer = "rbbg"
+    mm.guess = "rrbg"
+    mm.feedback
+
+    assert_equal 3, mm.positions
+  end
+
+  def test_it_keeps_up_with_the_guess_count
+    mm = Mastermind.new
+    mm.guess = "rrrr"
+    mm.guess_actions
+
+    assert_equal 1, mm.guess_count
+
+    mm.guess = "bbbb"
+    mm.guess_actions
+    mm.guess = "yyyy"
+    mm.guess_actions
+
+    assert_equal 3, mm.guess_count
   end
 end
+
+# def test_user_can_make_a_guess
+#   mm = Mastermind.new
+#   mm.("t")
+#   assert_equal "t" , mm.guess
+# end
+
+# def test_guess_from_user_input_is_case_insensitive
+#   mm = Mastermind.new
+#   mm.guess = "Q"
+#
+#   assert_equal "q", mm.guess
+# end
+
+#
+# def test_get_guess_method
+#   mm = Mastermind.new
+#
+# end
+
+
+
+# def test_feedback_prints_the_guess_upcased
+#   mm = Mastermind.new
+#   mm.get_guess("rbyg")
+#
+#   assert_equal "RBYG", mm.guess.upcase
+# end
